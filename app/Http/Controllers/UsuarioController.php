d416f4f6c63cca04e6de2f7b80388c3b9f27eae5
Esteban Carvajal <ecarvajal16@outlook.com>
2025-07-22 17:37:16 -0500

    Subiendo "usuarioController"

---
+++ app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use App\Models\Notificacion;
+
+class UsuarioController extends Controller
+{
+    public function index()
+    {
+        $usuario = Auth::user();
+
+        $notificacionesNoLeidas = Notificacion::where('paciente_id', Auth::id())
+                                      ->where('leido', false)
+                                      ->count();
+
+
+        return view('usuario', compact('notificacionesNoLeidas'));
+    }
+}
